d517ec8f7a7da072576894c2ec4b2cf428348bdd
Ângelo Savioti <matheussavioti@gmail.com>
2026-06-04 21:31:18 -0300

    feat: implement chord data initialization and update asset paths

--- lib/features/trainer/data/chord_data.dart
+++ lib/features/trainer/data/chord_data.dart
@@ -1,35 +1,48 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
 import '../domain/chord.dart';
 import '../domain/chord_type.dart';
 
-List<Chord> _chordList(ChordType type, String suffix) => [
-      Chord(symbol: 'C$suffix', type: type),
-      Chord(symbol: 'C#$suffix', altSymbol: 'Db$suffix', type: type),
-      Chord(symbol: 'D$suffix', type: type),
-      Chord(symbol: 'D#$suffix', altSymbol: 'Eb$suffix', type: type),
-      Chord(symbol: 'E$suffix', type: type),
-      Chord(symbol: 'F$suffix', type: type),
-      Chord(symbol: 'F#$suffix', altSymbol: 'Gb$suffix', type: type),
-      Chord(symbol: 'G$suffix', type: type),
-      Chord(symbol: 'G#$suffix', altSymbol: 'Ab$suffix', type: type),
-      Chord(symbol: 'A$suffix', type: type),
-      Chord(symbol: 'A#$suffix', altSymbol: 'Bb$suffix', type: type),
-      Chord(symbol: 'B$suffix', type: type),
-    ];
-
-final Map<ChordType, List<Chord>> chordData = {
-  ChordType.major:    _chordList(ChordType.major,    ''),
-  ChordType.minor:    _chordList(ChordType.minor,    'm'),
-  ChordType.aug:      _chordList(ChordType.aug,      'aug'),
-  ChordType.dim:      _chordList(ChordType.dim,      'dim'),
-  ChordType.sus:      _chordList(ChordType.sus,      'sus'),
-  ChordType.seventh:  _chordList(ChordType.seventh,  '7'),
-  ChordType.maj7:     _chordList(ChordType.maj7,     'maj7'),
-  ChordType.m7:       _chordList(ChordType.m7,       'm7'),
-  ChordType.dim7:     _chordList(ChordType.dim7,     'dim7'),
-  ChordType.halfDim7: _chordList(ChordType.halfDim7, 'm7b5'),
-  ChordType.mMaj7:    _chordList(ChordType.mMaj7,    'mMaj7'),
-  ChordType.augMaj7:  _chordList(ChordType.augMaj7,  'augMaj7'),
+const _qualityToType = <String, ChordType>{
+  'quality_major':    ChordType.major,
+  'quality_minor':    ChordType.minor,
+  'quality_aug':      ChordType.aug,
+  'quality_dim':      ChordType.dim,
+  'quality_sus4':     ChordType.sus,
+  'quality_seventh':  ChordType.seventh,
+  'quality_maj7':     ChordType.maj7,
+  'quality_m7':       ChordType.m7,
+  'quality_dim7':     ChordType.dim7,
+  'quality_halfdim7': ChordType.halfDim7,
+  'quality_mmaj7':    ChordType.mMaj7,
+  'quality_augmaj7':  ChordType.augMaj7,
 };
+
+Map<ChordType, List<Chord>> chordData = {};
+
+Future<void> initChordData() async {
+  final raw = await rootBundle.loadString('database/chords.json');
+  final list = jsonDecode(raw) as List<dynamic>;
+
+  final grouped = <ChordType, List<Chord>>{
+    for (final t in ChordType.values) t: [],
+  };
+
+  for (final entry in list) {
+    final type = _qualityToType[entry['qualityId'] as String];
+    if (type == null) continue;
+    final names = (entry['displayNames'] as List<dynamic>).cast<String>();
+    grouped[type]!.add(Chord(
+      symbol:    names[0],
+      altSymbol: names.length > 1 ? names[1] : null,
+      type:      type,
+    ));
+  }
+
+  chordData = grouped;
+}
 
 /// Returns the cumulative chord pool for levels 1..level (1-based).
 List<Chord> buildChordPool(int level) {
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app.dart';
+import 'features/trainer/data/chord_data.dart';
 import 'features/trainer/data/providers.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final prefs = await SharedPreferences.getInstance();
+  await initChordData();
   runApp(
     ProviderScope(
       overrides: [
